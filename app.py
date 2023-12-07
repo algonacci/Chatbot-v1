b0c5b2cfa196035951032db2d0f0ee028203a0d5
Eric Julianto <86970816+algonacci@users.noreply.github.com>
2023-12-07 10:52:30 +0700

    add API version

--- app.py
+++ app.py
@@ -1,47 +1,52 @@
+from flask import Flask, render_template, request, jsonify
+import random
+import json
+from keras.models import load_model
+import numpy as np
+import pickle
+from nltk.stem import WordNetLemmatizer
 import nltk
 nltk.download('popular')
-from nltk.stem import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
-import pickle
-import numpy as np
 
-from keras.models import load_model
 model = load_model('model.h5')
-import json
-import random
 intents = json.loads(open('data.json').read())
-words = pickle.load(open('texts.pkl','rb'))
-classes = pickle.load(open('labels.pkl','rb'))
+words = pickle.load(open('texts.pkl', 'rb'))
+classes = pickle.load(open('labels.pkl', 'rb'))
+
 
 def clean_up_sentence(sentence):
     # tokenize the pattern - split words into array
     sentence_words = nltk.word_tokenize(sentence)
     # stem each word - create short form for word
-    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    sentence_words = [lemmatizer.lemmatize(
+        word.lower()) for word in sentence_words]
     return sentence_words
 
 # return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
+
 
 def bow(sentence, words, show_details=True):
     # tokenize the pattern
     sentence_words = clean_up_sentence(sentence)
     # bag of words - matrix of N words, vocabulary matrix
-    bag = [0]*len(words)  
+    bag = [0]*len(words)
     for s in sentence_words:
-        for i,w in enumerate(words):
-            if w == s: 
+        for i, w in enumerate(words):
+            if w == s:
                 # assign 1 if current word is in the vocabulary position
                 bag[i] = 1
                 if show_details:
-                    print ("found in bag: %s" % w)
-    return(np.array(bag))
+                    print("found in bag: %s" % w)
+    return (np.array(bag))
+
 
 def predict_class(sentence, model):
     # filter out predictions below a threshold
-    p = bow(sentence, words,show_details=False)
+    p = bow(sentence, words, show_details=False)
     res = model.predict(np.array([p]))[0]
     ERROR_THRESHOLD = 0.25
-    results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
     # sort by strength of probability
     results.sort(key=lambda x: x[1], reverse=True)
     return_list = []
@@ -49,14 +54,17 @@ def predict_class(sentence, model):
         return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
     return return_list
 
+
 def getResponse(ints, intents_json):
     tag = ints[0]['intent']
     list_of_intents = intents_json['intents']
+    result = "No response found."  # Initialize result with a default response
     for i in list_of_intents:
-        if(i['tag']== tag):
+        if (i['tag'] == tag):
             result = random.choice(i['responses'])
             break
     return result
+
 
 def chatbot_response(msg):
     ints = predict_class(msg, model)
@@ -64,14 +72,38 @@ def chatbot_response(msg):
     return res
 
 
-from flask import Flask, render_template, request
-
 app = Flask(__name__)
 app.static_folder = 'static'
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/response", methods=["POST"])
+def chatbot_reponse_api():
+    if request.method == "POST":
+        input_data = request.get_json()
+        message = input_data["message"]
+        response = chatbot_response(message)
+        return jsonify({
+            "status": {
+                "code": 200,
+                "message": "Chatbot success responding",
+            },
+            "data": {
+                "response": response
+            }
+        }), 200
+    else:
+        return jsonify({
+            "status": {
+                "code": 405,
+                "message": "Method not allowed"
+            }
+        }), 405
+
 
 @app.route("/get")
 def get_bot_response():
--- training.py
+++ training.py
@@ -1,32 +1,33 @@
+import random
+# from tensorflow.keras.optimizers import Adam
+import tensorflow as tf
+from keras.layers import Dense, Activation, Dropout
+from keras.models import Sequential
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle
+import json
 import nltk
 from nltk.stem import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
-import json
-import pickle
 
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from keras.models import Sequential
-from keras.layers import Dense, Activation, Dropout
-from tensorflow.keras.optimizers import SGD
-import random
 
-words=[]
+words = []
 classes = []
 documents = []
 ignore_words = ['?', '!']
-data_file = open('data.json',encoding='utf-8').read()
+data_file = open('data.json', encoding='utf-8').read()
 intents = json.loads(data_file)
 
 
 for intent in intents['intents']:
     for pattern in intent['patterns']:
 
-        #tokenize each word
+        # tokenize each word
         w = nltk.word_tokenize(pattern)
         words.extend(w)
-        #add documents in the corpus
+        # add documents in the corpus
         documents.append((w, intent['tag']))
 
         # add to our classes list
@@ -34,20 +35,21 @@ for intent in intents['intents']:
             classes.append(intent['tag'])
 
 # lemmaztize and lower each word and remove duplicates
-words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
+words = [lemmatizer.lemmatize(w.lower())
+         for w in words if w not in ignore_words]
 words = sorted(list(set(words)))
 # sort classes
 classes = sorted(list(set(classes)))
 # documents = combination between patterns and intents
-print (len(documents), "documents")
+print(len(documents), "documents")
 # classes = intents
-print (len(classes), "classes", classes)
+print(len(classes), "classes", classes)
 # words = all words, vocabulary
-print (len(words), "unique lemmatized words", words)
+print(len(words), "unique lemmatized words", words)
 
 
-pickle.dump(words,open('texts.pkl','wb'))
-pickle.dump(classes,open('labels.pkl','wb'))
+pickle.dump(words, open('texts.pkl', 'wb'))
+pickle.dump(classes, open('labels.pkl', 'wb'))
 
 # create our training data
 training = []
@@ -60,25 +62,36 @@ for doc in documents:
     # list of tokenized words for the pattern
     pattern_words = doc[0]
     # lemmatize each word - create base word, in attempt to represent related words
-    pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern_words]
+    pattern_words = [lemmatizer.lemmatize(
+        word.lower()) for word in pattern_words]
     # create our bag of words array with 1, if word match found in current pattern
     for w in words:
         bag.append(1) if w in pattern_words else bag.append(0)
-    
+
     # output is a '0' for each tag and '1' for current tag (for each pattern)
     output_row = list(output_empty)
     output_row[classes.index(doc[1])] = 1
-    
+
     training.append([bag, output_row])
 # shuffle our features and turn into np.array
-random.shuffle(training)
-training = np.array(training)
-# create train and test lists. X - patterns, Y - intents
-X = list(training[:,0])
-Y = list(training[:,1])
+# Separating the bags and the output rows into two lists
+bags = [t[0] for t in training]
+outputs = [t[1] for t in training]
 
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=1)
-X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=0.2, random_state=1)
+# shuffle our features and labels in unison
+combined = list(zip(bags, outputs))
+random.shuffle(combined)
+bags, outputs = zip(*combined)
+
+# convert to np.array
+X = np.array(bags)
+Y = np.array(outputs)
+
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, Y, test_size=0.2, random_state=1)
+X_train, X_test, y_train, y_test = train_test_split(
+    X_train, y_train, test_size=0.2, random_state=1)
 print("Training data created")
 
 
@@ -92,11 +105,13 @@ model.add(Dropout(0.5))
 model.add(Dense(len(y_train[0]), activation='softmax'))
 
 # Compile model. Stochastic gradient descent with Nesterov accelerated gradient gives good results for this model
-sgd = SGD(lr=1, decay=1e-6, momentum=0.9, nesterov=True)
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+# sgd = tf.keras.optimizers.legacy.SGD()
+model.compile(loss='categorical_crossentropy',
+              optimizer=tf.keras.optimizers.Adam(), metrics=['accuracy'])
 
-#fitting and saving the model 
-history = model.fit(np.array(X_train), np.array(y_train), epochs=200, batch_size=32, verbose=1, validation_data=(X_test, y_test))
+# fitting and saving the model
+history = model.fit(np.array(X_train), np.array(
+    y_train), epochs=200, batch_size=32, verbose=1, validation_data=(X_test, y_test))
 model.save('model.h5', history)
 
 print(history.history.keys())
@@ -104,7 +119,7 @@ print(history.history.keys())
 loss_train = history.history['loss']
 loss_val = history.history['val_loss']
 epochs = range(200)
-%matplotlib inline
+# %matplotlib inline
 plt.plot(epochs, loss_train, 'g', label='Training loss')
 plt.plot(epochs, loss_val, 'b', label='validation loss')
 plt.title('Training and Validation loss')
